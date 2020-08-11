0e4f090848835d86572f9186c05e7586518f7f6f
johncoryk <coryk0789@gmail.com>
2020-08-11 11:08:08 -0400

    created db and offers table

--- db/migrations/202008111039_create_offers.sql
+++ db/migrations/202008111039_create_offers.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS offers (
   id SERIAL PRIMARY KEY,
-  user VARCHAR(255) NOT NULL,
   category VARCHAR(255) NOT NULL,
   description TEXT NOT NULL,
   time_offered INTEGER NOT NULL DEFAULT 1
